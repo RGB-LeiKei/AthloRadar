38bc544c51150abaaff34a0b9d6d287c70b014ae
LeiKei <92036191+RGB-LeiKei@users.noreply.github.com>
2026-01-16 21:05:46 +0100

    Add files via upload

---
+++ scripts/Slider.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+    if (document.getElementById("toggleSwitch").checked == true){
+        document.body.style.color = "white";
+        document.body.style.background = "#44344F";
+    } else {
+        document.body.style.color = "black";
+        document.body.style.background = "rgb(246, 255, 248)";
+    }
+}
