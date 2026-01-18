d89bdff31b0d97f3ac8e9c23c866d224acb56f02
LeiKei <92036191+RGB-LeiKei@users.noreply.github.com>
2026-01-18 23:58:21 +0100

    ver 0.4

--- scripts/Slider.js
+++ scripts/Slider.js
@@ -1,10 +1,10 @@
-function myFunction() {
+function SwitchTheme() {
 
     if (document.getElementById("toggleSwitch").checked == true){
         document.body.style.color = "white";
-        document.body.style.background = "#44344F";
+        document.body.style.background = "#2a2a30";
     } else {
         document.body.style.color = "black";
-        document.body.style.background = "rgb(246, 255, 248)";
+        document.body.style.background = "#f1fff2";
     }
 }
